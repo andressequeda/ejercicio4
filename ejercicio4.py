1a05726e552693e7ad519325c0fdb1f8f2266d6d
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 21:56:35 -0500

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,4 @@
+numero  =  float ( input ( "Ingrese un numero decimal:" ))
+parte_entera  =  int ( numero )
+parte_decimal  =  numero  -  parte_entera
+print ( "la parte entera es:" , parte_entera , "y la parte decimal es:" , parte_decimal )
